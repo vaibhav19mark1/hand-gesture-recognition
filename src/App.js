96afd10061708bd60ba88b2d1cd8a9c6b7ea87ab
vaibhav19mark1 <vaibhavtushar19@gmail.com>
2022-06-26 10:57:12 +0530

    hand detected and drawn

--- src/App.js
+++ src/App.js
@@ -1,22 +1,81 @@
-import logo from './logo.svg';
+import React, {useRef} from 'react';
+import * as tf from "@tensorflow/tfjs"
+import * as handpose from "@tensorflow-models/handpose"
+import Webcam from "react-webcam"
 import './App.css';
+import {drawHand} from './utilities';
 
-function App() {
-  return (
+function App(){
+  const webcamRef=useRef(null);
+  const canvasRef=useRef(null);
+
+  const runHandPose=async() => {
+    const net=await handpose.load();
+    console.log("Handpose model loaded.");
+    //Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    },100);
+  };
+
+  const detect=async(net) => {
+    //Check if data is available
+    if(typeof webcamRef.current !== "undefined" && webcamRef.current !== null &&
+      webcamRef.current.video.readyState===4){
+
+        //Get video properties
+        const video=webcamRef.current.video;
+        const videoWidth=webcamRef.current.video.videoWidth;
+        const videoHeight=webcamRef.current.video.videoHeight;
+
+        //Set video height and width
+        webcamRef.current.video.width=videoWidth;
+        webcamRef.current.video.height=videoHeight;
+
+        //Set canvas height and width
+        canvasRef.current.width=videoWidth;
+        canvasRef.current.height=videoHeight;
+
+        //Detect hand
+        const hand=await net.estimateHands(video);
+        console.log(hand);
+
+        //Draw mesh
+        const ctx = canvasRef.current.getContext("2d");
+        drawHand(hand,ctx);
+    }
+  }
+  runHandPose();
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam ref={webcamRef}
+        style={{
+          position:"absolute",
+          marginLeft:"auto",
+          marginRight:"auto",
+          left:0,
+          right:0,
+          textAlign:"center",
+          zindex:9,
+          width:640,
+          height:480
+        }}/>
+
+        <canvas
+        ref={canvasRef}
+        style={{
+          position:"absolute",
+          marginLeft:"auto",
+          marginRight:"auto",
+          left:0,
+          right:0,
+          textAlign:"center",
+          zindex:9,
+          width:640,
+          height:480
+        }}/>
+
       </header>
     </div>
   );
